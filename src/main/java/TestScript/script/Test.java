5977f9adeb4b9842b8747ed86862f1770992b935
osrsbots-com <contact@osrsbots.com>
2024-07-14 02:16:37 -0400

    Initial Commit

---
+++ src/main/java/TestScript/script/Test.java
@@ -0,0 +1,16 @@
+package script;
+
+import com.osrsbots.orb.scripts.framework.LoopingScript;
+import com.osrsbots.orb.scripts.framework.ScriptMeta;
+import util.Util;
+
+@ScriptMeta(author = "ORB", name = "YEET", version = 6.9)
+public class Test implements LoopingScript {
+    @Override
+    public int loop() {
+        Util.idk();
+
+
+        return 1000;
+    }
+}
---
+++ src/main/java/TestUtil/util/Util.java
@@ -0,0 +1,13 @@
+package util;
+
+public class Util {
+
+    public static boolean idk(){
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("dir=" + System.getProperty("user.dir"));
+
+    }
+}
